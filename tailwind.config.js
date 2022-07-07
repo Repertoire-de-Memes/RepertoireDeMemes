772240bddfc386f8163e54615b79da55d7f93ccb
NetherMC <thelio.trinite@outlook.com>
2022-07-07 15:20:08 +0200

    packages + tailwind

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./pages/**/*.html', './pages/*.html', './src/*.ts'],
-  theme: {
-    darkMode: 'media',
-    extend: {},
-  },
-  plugins: [],
-}
