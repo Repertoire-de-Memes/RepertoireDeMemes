433f25d0aaf3f2b285c5f069d5a00513a835f3a6
NetherMC <thelio.trinite@outlook.com>
2022-07-07 17:08:53 +0200

    tabulation

--- src/index.ts
+++ src/index.ts
@@ -21,13 +21,13 @@ app.get('/api/images', (req, res) => {
   const query = req.query.q;
 
   if (!query) {
-  res.send(
-    fs.readdirSync(path.resolve(`${__dirname}/../server/assets/images`)).map(img => ({
-      file: `/images/${img}`,
-      filename: filenames[img.replace(/.(jpg|png|gif)/g, '')] + '.jpg',
-      keywords: keywords[img.replace(/.(jpg|png|gif)/g, '')]
-    }))
-  );
+    res.send(
+      fs.readdirSync(path.resolve(`${__dirname}/../server/assets/images`)).map(img => ({
+        file: `/images/${img}`,
+        filename: filenames[img.replace(/.(jpg|png|gif)/g, '')] + '.jpg',
+        keywords: keywords[img.replace(/.(jpg|png|gif)/g, '')]
+      }))
+    );
   } else {
     res.send(fs.readdirSync(path.resolve(`${__dirname}/../server/assets/images`))
       .filter(img => {
