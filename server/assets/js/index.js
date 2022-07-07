6071e550275beb5f3c6134365d2708024a68d1e0
NetherMC <thelio.trinite@outlook.com>
2022-07-07 16:51:54 +0200

    webpage

--- server/assets/js/index.js
+++ server/assets/js/index.js
@@ -2,20 +2,28 @@ const searchInput = document.querySelector('#q');
 const searchResults = document.querySelector('#search-results');
 
 searchInput?.addEventListener('input', (e) => {
-  console.log(e.target.value);
+  setSearchResults(e.target.value);
 });
 
 const getSearchResults = (query) => {
-  return fetch('/api/images').then(r => r.json());
+  return fetch(`/api/images${query && `?q=${encodeURIComponent(query)}` || ''}`).then(r => r.json());
 }
 
-const searchResultsJson = getSearchResults();
-searchResultsJson.then(results => {
-  results.map(result => {
-    const elem = document.createElement('div');
-    elem.classList.add('col-3', 'margin-2', 'align-items-center', 'd-flex');
+const setSearchResults = (query) => {
+  getSearchResults(query).then(results => {
+    searchResults.innerHTML = '';
 
-    elem.innerHTML = `<a href="${result.file}" download="${result.filename}"><img src="${result.file}" class="img-fluid" />`;
-    searchResults?.appendChild(elem);
+    results.map(result => {
+      const elem = document.createElement('div');
+      elem.classList.add('col-3', 'my-1', 'align-items-center', 'd-flex');
+
+      elem.innerHTML = `
+      <a href="${result.file}" download="${result.filename}">
+        <img src="${result.file}" class="img-fluid" />
+      </a>`;
+      searchResults?.appendChild(elem);
+    });
   });
-});
+}
+
+setSearchResults();
