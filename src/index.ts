a3dc4738f8711e8f3f37af610f60ad367e1131fe
NetherMC <thelio.trinite@outlook.com>
2022-07-07 17:06:12 +0200

    enfin la recherche marche bordel de merde

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/images', (req, res) => {
-  console.log(keywords);
-  
+  const query = req.query.q;
+
+  if (!query) {
   res.send(
     fs.readdirSync(path.resolve(`${__dirname}/../server/assets/images`)).map(img => ({
       file: `/images/${img}`,
@@ -27,6 +28,29 @@ app.get('/api/images', (req, res) => {
       keywords: keywords[img.replace(/.(jpg|png|gif)/g, '')]
     }))
   );
+  } else {
+    res.send(fs.readdirSync(path.resolve(`${__dirname}/../server/assets/images`))
+      .filter(img => {
+        const keywordsForThisImg = (keywords[img.replace(/.(jpg|png|gif)/g, '')] || []).join(' ');
+        return keywordsForThisImg.includes(query);
+      })
+      .map(img => ({
+        file: `/images/${img}`,
+        filename: filenames[img.replace(/.(jpg|png|gif)/g, '')] + '.jpg',
+        keywords: keywords[img.replace(/.(jpg|png|gif)/g, '')]
+      }))
+    )
+  }
+});
+
+app.get('/css/bootstrap.min.css', (req, res) => {
+  res.setHeader('Content-Type', 'text/css');
+  res.sendFile(path.resolve(`${__dirname}/../node_modules/bootstrap/dist/css/bootstrap.min.css`));
+});
+
+app.get('/css/app.css', (req, res) => {
+  res.setHeader('Content-Type', 'text/css');
+  res.sendFile(path.resolve(`${__dirname}/../server/assets/css/app.css`));
 });
 
 app.use(express.static(path.resolve(`${__dirname}/../server/assets`)));
